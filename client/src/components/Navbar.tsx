9f409920f304cce90a0ee81c99bcd24be081ec6c
Goutham Korla <goutham.korla@infor.com>
2026-01-07 23:09:40 +0530

    Added responsive Header & SideBar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import {assets} from "../assets/assets.js"
+import { MenuIcon, SearchIcon, XIcon } from 'lucide-react'
 const Navbar = () => {
+let [open,setisOpen]=useState(false);
   return (
-    <div className='fixed top-0 left-0 flex items-center justify-between px-6 py-5 '>
-        <Link to={'/'}  className='max-md:flex-1'>
-         <img src={assets.logo} className='w-10 h-auto '/>
+   <div className='flex items-center justify-between px-6 py-5 md:px-16 lg:px-36'>
+<Link to ="/">
+<img src={assets.logo} className='w-36 h-auto'/>
+</Link>
 
-        </Link>
-        {/* Menu Part */}
-        <div></div>
+<div className={`max-md:font-medium max-md:text-lg flex flex-col items-center  md:flex-row
+max-md:justify-center gap-8 px-8 py-3 max-md:h-screen  md:rounded-full backdrop-blur bg-black/60
+md:bg-white/10 md:border  md:border-gray-300/20 overflow-hidden transition-[translate] duration-300
+max-md:absolute max-md:top-0 max-md:left-0 z-50  ${open ? "max-md:translate-x-0 w-full" : "max-md:-translate-x-full"}`}>
+ 
+    <XIcon onClick={()=>setisOpen(!open)} className='md:hidden absolute right-6 top-6 w-6 h-6 cursor-pointer'/>
+    <Link onClick={()=>{scrollTo(0,0); setisOpen(false)}} to={"/"}>Home</Link>
+    <Link  onClick={()=>{scrollTo(0,0); setisOpen(false)}} to={"/movies"}>Movies</Link>
+    <Link onClick={()=>{scrollTo(0,0); setisOpen(false)}}  to={"/"}>Theaters</Link>
+    <Link  onClick={()=>{scrollTo(0,0); setisOpen(false)}} to={"/"}>Releases</Link>
+    <Link  onClick={()=>{scrollTo(0,0); setisOpen(false)}} to={"/favourites"}>Favourites</Link>
+</div>
 
-        </div>
+<div className= { `flex gap-8 
+    items-center `}>
+<SearchIcon className='max-md:hidden cursor-pointer w-6 h-6'/>
+<button className='px-4 py-1 sm:px-7 sm:py-2 bg-red-700 hover:bg-red-900 transition duration-200 rounded-full '>Login</button>
+</div>
+   <MenuIcon onClick={()=>{setisOpen(true)}} className={`w-8 h-8 md:hidden cursor-pointer max-md:ml-4` }/>
+   
+   </div>
   )
 }
 
